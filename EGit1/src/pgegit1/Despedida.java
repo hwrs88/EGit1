d3509474b18ffdaad2d855d392a0fa053673564f
William <William@William-PC>
2013-10-24 22:19:14 +0200

    CD6

--- EGit1/src/pgegit1/Despedida.java
+++ EGit1/src/pgegit1/Despedida.java
@@ -4,6 +4,12 @@ public class Despedida {
 	
 	public static void despedida(){
 		
+		System.out.println("Bye bye...");
+		
+	}
+	
+	public static void adios(){
+		
 		System.out.println("Adios...");
 		
 	}
