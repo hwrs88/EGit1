b10ee2b7f9e8fe6fe6b8414f36970ed1eaa27078
William <William@William-PC>
2013-10-24 22:06:41 +0200

    CR1.02

--- EGit1/src/pgegit1/Saludo.java
+++ EGit1/src/pgegit1/Saludo.java
@@ -1,23 +1,33 @@
 package pgegit1;
 
 public class Saludo {
-	
-	public static void hola(){
-		
+
+	public static void hola() {
+
 		System.out.println("Hola");
-		
+
 	}
-	
-	public static void hello(){
-		
+
+	public static void hello() {
+
 		System.out.println("Hello");
-		
+
 	}
-	
-	public static void holaTodos(){
-		
+
+	public static void holaTodos() {
+
 		System.out.println("Hola Todos!");
-		
+
+	}
+
+	public static void buenDia(boolean generalizado) {
+
+		if (generalizado) {
+			System.out.println("Buenas tardes");
+		} else {
+			System.out.println("Buen dia");
+		}
+
 	}
 	
 }
